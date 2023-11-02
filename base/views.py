657fa6fde8e530b74b22f7bbbbd8539a6ccaadac
SuruBasnet <surubasnet824@gmail.com>
2023-11-02 08:49:37 +0545

    edit part done on both templates and views

--- base/views.py
+++ base/views.py
@@ -18,5 +18,20 @@ def create(request):
         ToDo.objects.create(name=name,description=description,status=status)
 
         return redirect('home')
+    content = {'method' : 'create'}
+    return render(request,'create.html',context=content)
 
-    return render(request,'create.html')
+def edit(request,pk):
+    todo = ToDo.objects.get(id=pk)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        todo.name = name
+        todo.description = description
+        todo.status = status
+        todo.save()
+
+        return redirect('home')
+    content = {'method':'edit','todo':todo}
+    return render(request,'create.html',context=content)
